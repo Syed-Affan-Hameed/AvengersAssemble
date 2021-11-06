992c4884689a1ff415fd8542bde1d74f5743325e
Syed_Affan_Hameed <syedaffanhameed@gmail.com>
2021-11-06 20:12:21 +0530

    Second Commit

--- app/src/main/java/com/syed/regraspingandroid/LoginActivity.kt
+++ app/src/main/java/com/syed/regraspingandroid/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.syed.regraspingandroid
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -15,6 +17,8 @@ class LoginActivity : AppCompatActivity()  {
     val validmobilenum= arrayListOf<String>("1127878221","112121221","1121214544","3321214544","3329921454","3327714544")
     val validpass= arrayListOf<String>("tonystark","capstark","romanoff","iamangry","hawkeye","jane")
     var avengerName= arrayOf("Iron Man","Captain America","Black Widow","Gladiator Hulk","HawkEye Ronin","Thor Odinson")
+    var LoginStatus=false
+    lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -25,7 +29,15 @@ class LoginActivity : AppCompatActivity()  {
         btnlogin = findViewById(R.id.btnlogin)
         txtforgot = findViewById(R.id.txtforgot)
         txtreg = findViewById(R.id.txtreg)
+        sharedPreferences=getSharedPreferences(getString(R.string.preference_file_name), Context.MODE_PRIVATE)
 
+        if(LoginStatus){
+            val intent=Intent(this@LoginActivity,MainActivity::class.java)
+            startActivity(intent)
+        }
+        else{
+            setContentView(R.layout.activity_login)
+        }
         btnlogin.setOnClickListener{
 
             var mob=etmobilenum.text.toString()
@@ -36,6 +48,7 @@ class LoginActivity : AppCompatActivity()  {
             {
                 for(i in 0 until validpass.size){
                     if(pass==validpass[i]){
+                        savepreferences()
                         intent.putExtra("Name",avengerName[i])
                         startActivity(intent)
                         finish()
@@ -53,7 +66,9 @@ class LoginActivity : AppCompatActivity()  {
 
     }
 
-
+ fun savepreferences(){
+     sharedPreferences.edit().putBoolean("LoginStatus",true).apply()
+ }
 
 
 }
